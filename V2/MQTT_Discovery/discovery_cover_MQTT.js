db25f782a43d99ee3327587dde223877e1110035
coselig_office <coseligtest@gmail.com>
2025-12-15 15:32:59 +0800

    2025/12/15 站存，窗簾

--- V2/MQTT_Discovery/discovery_cover_MQTT.js
+++ V2/MQTT_Discovery/discovery_cover_MQTT.js
@@ -2,21 +2,53 @@
 // 支援: 窗簾(curtain), 捲簾, 排煙窗
 
 // ============ 窗簾設備定義 ============
-let covers = [
-    { path: "curtain/21/1-2-3", name: "鐵捲門" },
-    { path: "curtain/22/1-2", name: "會議室捲簾" },
-    { path: "curtain/23/1-2", name: "布簾" },
-    { path: "curtain/23/3-4", name: "沙簾" },
-    { path: "curtain/23/5-6-7", name: "排煙窗" },
+let devices = msg.devices[
+    { type: "cover", module_id: 21, channel: "1-2-3", name: "鐵捲門" },
+    { type: "cover", module_id: 22, channel: "1-2", name: "會議室捲簾" },
+    { type: "cover", module_id: 23, channel: "1-2", name: "布簾" },
+    { type: "cover", module_id: 23, channel: "3-4", name: "沙簾" },
+    { type: "cover", module_id: 23, channel: "5-6-7", name: "排煙窗" }
 ];
 
 
 // ============ 註冊/清除邏輯 ============
+
 let msgs;
 
 if (msg.action === "clear") {
+    let msgs = devices.map(dev => {
+        let path = `${dev.type}/${dev.module_id}/${dev.channel}`;
+        let unique_id = `${dev.type}_${dev.module_id}_${dev.channel}`;
+        let topic = `homeassistant/cover/${unique_id}/config`;
+        let payload;
+    });
+} else if (msg.action === "add") {
+    payload = {
+        name: dev.name,
+        unique_id: unique_id,
+        retain: true,
+        optimistic: true
+    };
+
+    switch (dev.channel.split("-").length) {
+        case 2: {
+            payload.command_topic = `homeassistant/cover/${dev.type}/${dev.module_id}/${dev.channel}/oc/set`;
+            break;
+        }
+        case 3: {
+            payload.command_topic = `homeassistant/cover/${dev.type}/${dev.module_id}/${dev.channel}/ocs/set`;
+            break;
+        }
+        default: {
+            node.warn("Unknown type of curtain");
+            break;
+        }
+    }
+}
+
+if (msg.action === "clear") {
     // 強制全部清除
-    msgs = covers.map(cover => {
+    msgs = devices.map(cover => {
         let uid = cover.path.replace(/\//g, "_");
         return {
             topic: `homeassistant/cover/${uid}/config`,
@@ -26,7 +58,7 @@ if (msg.action === "clear") {
     });
 } else if (msg.action === "add") {
     // 新增註冊
-    msgs = covers.map(cover => {
+    msgs = devices.map(cover => {
         let uid = cover.path.replace(/\//g, "_");
         let part = cover.path.split("/");
         let device_type = part[0];
